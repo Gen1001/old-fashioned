cfe90879b8b6d38ce68e92bba8c7b8da56233cc7
Gen1001 <genshiro13@gmail.com>
2024-07-12 23:42:03 +0900

    commit

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -3,8 +3,6 @@
  const cropBtn = document.getElementById('crop-btn');
  const userForm = document.getElementById('userForm');
  
- let cropper;
- 
  imageInput.addEventListener('change', () => {
    if (imageInput.files[0]) {
      let fileReader = new FileReader();
@@ -30,8 +28,14 @@
    }
  })
  
+ document.getElementById('imageFile').addEventListener('chan')
+ 
  //トリミングした画像を追加する
 cropBtn.addEventListener('click', function (event) {
+	if (imageInput.files.length === 0) {
+		userForm.submit();
+		return;
+	}
 	event.preventDefault();
   	if (cropper) {
     // トリミングされた画像データを取得
@@ -47,7 +51,5 @@ cropBtn.addEventListener('click', function (event) {
 		userForm.submit();
 	}, 'image/jpeg')
    
-  }　else {
-	userForm.submit();
   }
 });
