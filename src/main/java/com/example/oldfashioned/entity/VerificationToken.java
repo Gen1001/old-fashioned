cdbdeb1582ebd15f7cc1174ff29bac71a19ae213
Gen1001 <genshiro13@gmail.com>
2024-07-15 12:43:23 +0900

    fetch

--- src/main/java/com/example/oldfashioned/entity/VerificationToken.java
+++ src/main/java/com/example/oldfashioned/entity/VerificationToken.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +22,7 @@ public class VerificationToken {
 	@Column(name = "id")
 	private Integer id;
 	
-	@OneToOne
+	@OneToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "user_id")
 	private User user;
 	
