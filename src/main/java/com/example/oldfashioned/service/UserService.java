e7931a4af0d8b094887d674c45e4c7dac433c06f
Gen1001 <genshiro13@gmail.com>
2024-07-21 13:06:28 +0900

    get address

--- src/main/java/com/example/oldfashioned/service/UserService.java
+++ src/main/java/com/example/oldfashioned/service/UserService.java
@@ -15,7 +15,7 @@ import com.example.oldfashioned.form.UserEditForm;
 import com.example.oldfashioned.repository.RoleRepository;
 import com.example.oldfashioned.repository.UserRepository;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -41,7 +41,7 @@ public class UserService {
 		//AWSのS3クライアントを初期化する
         this.s3Client = S3Client.builder()
                 .region(Region.AP_SOUTHEAST_2)
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 
